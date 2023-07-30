f16aa89fdbfe0fc8dcc0222b52f0934c5e10501f
phamdinhthe2509@gmail.com <phamdinhthe2509@gmail.com>
2023-07-30 23:28:39 +0700

    commit docker file

--- src/main/java/com/example/demomicroservice/AuthorServiceApplication.java
+++ src/main/java/com/example/demomicroservice/AuthorServiceApplication.java
@@ -1,25 +1,65 @@
 package com.example.demomicroservice;
 
+import com.example.demomicroservice.config.web.ApplicationContextProvider;
 import com.example.demomicroservice.service.AuthorService;
+import com.the.common.constant.rabbitMQ.RabbitMQConstant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class AuthorServiceApplication {
 
-  private final static Logger LOGGER = LoggerFactory.getLogger(AuthorServiceApplication.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(AuthorServiceApplication.class);
 
-  public static void main(String[] args) {
-    SpringApplication.run(AuthorServiceApplication.class, args);
-    LOGGER.error("Information user euruka");
-    LOGGER.error("Information user euruka");
-    LOGGER.error("Information user euruka");
-  }
+//    @Resource(name = "RabbitTemplate")
+//    private AmqpTemplate rabbitTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorServiceApplication.class, args);
+
+        AuthorServiceApplication authorServiceApplication = new AuthorServiceApplication();
+
+        LOGGER.info("start ----------------------------- start");
+        authorServiceApplication.test();
+    }
+
+    public void test() {
+        ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
+        AmqpTemplate rabbitTemplate = (RabbitTemplate) applicationContext.getBean("RabbitTemplate");
+        List<String> list = List.of("1","2","3");
+        String receive = (String) rabbitTemplate.convertSendAndReceive(RabbitMQConstant.Queue.QUEUE_AUTHOR, RabbitMQConstant.Routing.ROUTING_AUTHOR, list);
+        System.out.println("Received after send " + receive);
+
+        System.out.println("------------end-------------");
+
+    }
+
+    @RabbitListener(queues = RabbitMQConstant.Queue.QUEUE_AUTHOR)
+    public String receiveMessageFromQueue1(List<String> list) throws InterruptedException {
+        System.out.println("Received message from Queue 1: " + list.size());
+
+        Thread.sleep(3000);
+        return list.size() + "ok ban nha";
+    }
+
+    @RabbitListener(queues = RabbitMQConstant.Queue.QUEUE_AUTHOR)
+    public String receiveMessageFromQueue2(String message) throws InterruptedException {
+        System.out.println("Received message from Queue 2: " + message);
+
+        Thread.sleep(3000);
+        return message + " ok nha toi la queue 2";
+    }
 }
---
+++ src/main/java/com/example/demomicroservice/config/rabbitMQ/RabbitMQConfig.java
@@ -0,0 +1,61 @@
+package com.example.demomicroservice.config.rabbitMQ;
+
+import com.the.common.constant.rabbitMQ.RabbitMQConstant;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("192.168.1.106");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("ricky");
+        connectionFactory.setPassword("25092002");
+        return connectionFactory;
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(RabbitMQConstant.Queue.QUEUE_AUTHOR, false);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(RabbitMQConstant.Exchange.EXCHANGE_AUTHOR);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(RabbitMQConstant.Routing.ROUTING_AUTHOR);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean(name = "RabbitTemplate")
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            if (!ack) {
+                System.out.println("Failed to send message: " + cause);
+                // Implement your retry or error handling logic here
+            } else {
+                System.out.println("Message sent successfully.");
+            }
+        });
+        return rabbitTemplate;
+    }
+}
